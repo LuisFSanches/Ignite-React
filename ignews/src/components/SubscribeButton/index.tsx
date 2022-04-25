22a2947f19117ac5fd2f87ab59437a0312be8ff0
LuisFSanches <luis-lipe@hotmail.com>
2022-04-25 18:40:41 -0300

    Finalização das configurações do stripe e login

--- ignews/src/components/SubscribeButton/index.tsx
+++ ignews/src/components/SubscribeButton/index.tsx
@@ -1,13 +1,43 @@
 
+import { useSession, signIn } from 'next-auth/react'
+import { api } from '../../services/api'
+import { getStripeJs } from '../../services/stripe-js'
 import styles from './styles.module.scss'
+
+//getServerSideProps(SSR) -->Executado no momento que a página é renderizada
+//getStaticProps(SSG) -->Executado no momento que a página é renderizada
+//API routes --> A partir da ação do usuário
 
 interface SubscribeButtonProps{
     priceId: string
 }
 
 export function SubscribeButton({priceId}:SubscribeButtonProps){
+
+    const {data:session} = useSession()
+
+    async function handleSubscription(){
+        if(!session){
+            signIn('github')
+            return
+        }
+        try{
+            const response = await api.post('/subscribe')
+
+            const {sessionId} = response.data;
+            const stripe = await getStripeJs()
+
+            await stripe.redirectToCheckout({sessionId})
+
+        }catch(error){
+            alert(error.message)
+        }
+        
+        
+    }
+
     return (
-        <button type="button" className={styles.subscribeButton}>
+        <button type="button" className={styles.subscribeButton} onClick={handleSubscription}>
             Subscribe now
         </button>
     )
--- ignews/src/pages/api/auth/[...nextauth].ts
+++ ignews/src/pages/api/auth/[...nextauth].ts
@@ -22,12 +22,30 @@ export default NextAuth({
   callbacks:{
       async signIn({user, account, profile}){
           const {email} = user
+
           try{
             await fauna.query(
+              query.If(
+                query.Not(
+                  query.Exists(
+                    query.Match(
+                      query.Index('user_by_email'),
+                      query.Casefold(user.email)
+                    )
+                  )
+                ),
                 query.Create(
-                    query.Collection('users'),
-                    {data: {email}}
+                  query.Collection('users'),
+                  {data: {email}}
+                ),
+                query.Get(
+                  query.Match(
+                    query.Index('user_by_email'),
+                    query.Casefold(user.email)
+                  )
                 )
+              )
+       
             )
             return true
           } catch(error){
